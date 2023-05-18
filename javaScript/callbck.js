6261dfce9c2bf6c23bcab8fe08cc2249eb1f2e04
Hitesh Saini <hitesh9694saini@gmail.com>
2023-05-18 14:20:07 +0530

    first day

--- javaScript/callbck.js
+++ javaScript/callbck.js
@@ -1,19 +1,19 @@
-const calculate = (a,b,addition)=>{
-    return addition(a,b);
-}
+// const calculate = (a,b,addition)=>{
+//     return addition(a,b);
+// }
 
 // function addition(a,b){return a+b;}
 
-const addition= (a,b)=>a+b;
+// const addition= (a,b)=>a+b;
 
-console.log(calculate(2,3,addition));
-setTimeout(function(){
-    console.log("heel");
-},  4000 );
+// console.log(calculate(2,3,addition));
+// setTimeout(function(){
+//     console.log("heel");
+// },  4000 );
 
 
-console.log("fgchbnj");
-console.log("ertyu");
+// console.log("fgchbnj");
+// console.log("ertyu");
 /**
  * let c = 3;
 let d = 4;
@@ -24,4 +24,31 @@ function sum( a , b) {
     return 5;
 }
  */
+
+
+function getCheese(callback){
+
+    setTimeout(()=>{
+       const cheese = "🧀";
+       console.log("here is cheese ",cheese);
+      callback(cheese);
+    },2000);
+
+   
+}
+
+function makeDough(cheese){
+
+    setTimeout(()=>{
+
+        const dough = cheese+"";
+        
+    },2000);
+
+}
+
+getCheese((cheese)=>{
+    console.log("got the cheese ",cheese);
+});
+
 
